609e54192ca98d0cb30f9aa0875e13382bf8cdd1
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-03 01:42:31 +0530

    Program to convert number from octal,binary and hexadecimal  number into decimal number

---
+++ Program2.py
@@ -0,0 +1,13 @@
+# Program to convert number from octal,binary and hexadecimal
+# number into decimal number
+
+n1=0o17
+n2=0b1110010
+n3=0x1c2
+
+n=int(n1)
+print('Octal 17 =',n)
+n=int(n2)
+print('Binary 1110010 =',n)
+n=int(n3)
+print('Hexcadecimal 1c2 =',n)
