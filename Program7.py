8f4dcef97a4c5e6f556dcc8645662435addc6e48
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-08 21:24:14 +0530

    A Program to accept single character,integer and float  number as a input and display it

---
+++ Program7.py
@@ -0,0 +1,10 @@
+# A Program to accept single character,integer and float
+# number as a input and display it
+
+ch=input('Enter the character:')
+int=int(input('Enter the integer number:'))
+float=float(input('Enter the float number:'))
+
+print('\nThe Entered character is:',ch[0])
+print('The Entered integer number is:',int)
+print('The Entered float number is:',float)
