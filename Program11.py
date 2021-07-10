48834e2ddcee6f6ed247ec87139d2d11cce54004
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-10 23:18:46 +0530

    A program to accept a strings in a same line seperated by comma. using split() function

---
+++ Program11.py
@@ -0,0 +1,5 @@
+# A program to accept a strings in a same line seperated by comma.
+
+lst=[x for x in input("Enter the strings spearated by comma :").split(',')]
+
+print(' Entered strings are\n',lst)
