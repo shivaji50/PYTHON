bf8389cc3d5f1ee658179fc328b54782d67047d6
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-10 23:16:38 +0530

    Accept 3 integer in a same line and display its sum

---
+++ Program9.py
@@ -0,0 +1,5 @@
+# Accept 3 integer in a same line and display its sum
+
+var1,var2,var3=[int(x) for x in input("Enter 3 number spearted by atleast one space:").split()]
+
+print('Sum =',var1+var2+var3)
