27ee78012d1352d715bacd60d14376cbf4cd44e0
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-12 21:24:09 +0530

    A Python program to know if a given number is zero,negative or positive (Using if.....elif...else statement)

---
+++ Program20.py
@@ -0,0 +1,10 @@
+# A Python program to know if a given number is zero,negative or positive (Using if.....elif...else statement)
+
+num=int(input('Enter the number to check :'))
+
+if num == 0:
+	print("The number is zero")
+elif num > 0:
+	print("The number is positive")
+else:
+	print("The number is negative")
