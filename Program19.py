2e16ee4938b1e8d6837e9241d93085ea3289479d
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-07-12 21:23:47 +0530

    A python program to accept a number from the user and check its even or odd.(Using if...else statement)

---
+++ Program19.py
@@ -0,0 +1,8 @@
+# A python program to accept a number from the user and check its even or odd.(Using if...else statement)
+
+x=int(input('Enter the number :'))
+
+if x % 2 == 0:
+	print(x,"is even number")
+else: 
+    print(x,"is odd number")	
