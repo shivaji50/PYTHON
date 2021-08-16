8a24bb785071bdaf2d25caf15bf238f376a26413
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-08-16 23:55:23 +0530

    A program to understand how a function return multiple value

---
+++ Program69.py
@@ -0,0 +1,15 @@
+# A program to understand how a function return multiple value
+def sum(a,b):
+    c=a+b
+    d=a-b
+    return c,d
+
+
+
+
+
+
+
+x,y=sum(10,5)
+print("Addition is :",x)
+print("Substraction is :",y)    
