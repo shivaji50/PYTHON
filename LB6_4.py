9150a61ac87480a97ef6d9a89b199799dc3fc3ed
Shivaji <82969117+shivaji50@users.noreply.github.com>
2021-08-24 23:51:57 +0530

    a program which accept number from user and count frequency of 4 in it.

---
+++ LB6_4.py
@@ -0,0 +1,29 @@
+# a program which accept number from user and count frequency of 4 in it.  
+
+# Input  : 2395
+# Output : 0
+# Function name : Digit()
+# Author : Shivaji Das
+# Date :  24 august 2021
+
+def Digit(no):
+    cnt=0
+    if no < 0:
+        no=-(no)
+    while no > 0:
+        digit=int(no%10)
+        if digit==4:
+            cnt=cnt+1
+        no=int(no/10)
+    
+    return cnt
+
+
+def main():
+    num=int(input("Enter the number :"))
+    ret=Digit(num)
+    print("The Frequency of 4 is :",ret)
+
+
+if __name__=="__main__":
+    main()     
